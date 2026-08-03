6153eae41adc7baf3fb1591d6b77620b7c4379b9
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2026-08-03 17:59:30 +0500

    connect sqlite3, add types node

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,22 @@
-import express from 'express'
-import { PORT } from './config'
-import { cardsRouter } from './routers/cards.router'
+import express from 'express';
+import { PORT } from './config';
+import { createTables } from './database/create-tables';
+import { cardsRouter } from './routers/cards.router';
 
+async function run() {
+  await createTables();
 
-const server = express();
+  const server = express();
 
-server.get('/',(req, res) => {
- res.send('HELLO')
-})
+  server.use(express.json());
 
-server.use('/cards', cardsRouter)
+  server.get('/', (req, res) => {
+    res.send('HELLO');
+  });
 
-server.listen(PORT);
+  server.use('/cards', cardsRouter);
+
+  server.listen(PORT);
+}
+
+run().catch((error) => console.error(error));
--- src/routers/cards.router.ts
+++ src/routers/cards.router.ts
@@ -1,25 +1,73 @@
-import express, { Request, Response } from 'express'
-import { Card, CreateCardRequest, GetCardsResponse } from '../types/cards'
-import { IdParams } from '../types/common'
+import { randomUUID } from 'crypto';
+import express, { Request, Response } from 'express';
+import {
+  createCard,
+  deleteCard,
+  getManyCard,
+  getOneCard,
+  updateCard,
+} from '../database/cards-repository';
+import { Card, CreateCardRequest, GetCardsResponse } from '../types/cards';
+import { IdParams } from '../types/common';
 
-export const cardsRouter = express.Router()
+export const cardsRouter = express.Router();
 
-cardsRouter.get('/', (req: Request<{}, {}>, res: Response<GetCardsResponse>) => {
-  // TODO: Return cards
-})
+cardsRouter.get(
+  '/',
+  async (req: Request<{}, {}>, res: Response<GetCardsResponse>) => {
+    const cards = await getManyCard();
+    res.send(cards);
+  },
+);
 
-cardsRouter.get('/:id', (req: Request<IdParams, {}>, res: Response<Card>) => {
-  // TODO: Return card
-})
+cardsRouter.get(
+  '/:id',
+  async (req: Request<IdParams, {}>, res: Response<Card | string>) => {
+    const card = await getOneCard(req.params.id);
 
-cardsRouter.post('/', (req: Request<{}, CreateCardRequest>, res: Response<Card>) => {
-  // TODO: Create card
-})
+    if (!card) {
+      res.status(404).send('Card not found ');
+      return;
+    }
 
-cardsRouter.put('/:id', (req: Request<IdParams, Card>, res: Response<Card>) => {
-  // TODO: Update card
-})
+    res.send(card);
+  },
+);
 
-cardsRouter.delete('/:id', (req: Request<IdParams>, res: Response<void>) => {
-  // TODO: Delete card
-})
+cardsRouter.post(
+  '/',
+  async (req: Request<{}, Card, CreateCardRequest>, res: Response<Card>) => {
+    const card: Card = {
+      text: req.body.text,
+      id: randomUUID(),
+    };
+
+    await createCard(card);
+
+    res.send(card);
+  },
+);
+
+cardsRouter.put(
+  '/:id',
+  async (
+    req: Request<IdParams, Card, CreateCardRequest>,
+    res: Response<Card>,
+  ) => {
+    const card = {
+      id: req.params.id,
+      text: req.body.text,
+    };
+
+    await updateCard(card);
+    res.send(card);
+  },
+);
+
+cardsRouter.delete(
+  '/:id',
+  async (req: Request<IdParams>, res: Response<void>) => {
+    await deleteCard(req.params.id);
+    res.sendStatus(204);
+  },
+);
